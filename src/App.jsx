a2841210e03e1aac4ae7c516cb662cae33c9ec0f
Robert Bezerra <robertbezerrads@gmail.com>
2022-11-21 17:58:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export default function App({ transferToPerson }) {
+	const [accountBalance, setAccountBalance] = useState(5000);
+	const [transferAmount, setTransferAmount] = useState(0);
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		setTimeout(() => {
+			setAccountBalance((prev) => prev - transferAmount);
+			setTransferAmount(0);
+		}, 200);
+	};
+
+	return (
+		<>
+			<h1>Account</h1>
+
+			<p>Current account balance: {accountBalance}</p>
+
+			<p>Send money to: {transferToPerson}</p>
+			<form onSubmit={onSubmit}>
+				<label htmlFor="transferAmount">Transfer Amount</label>
+				<br />
+				<input
+					min="0"
+					type="number"
+					id="transferAmount"
+					value={transferAmount}
+					onChange={(e) => setTransferAmount(e.target.value)}
+				/>
+				<button type="submit">Send</button>
+			</form>
+		</>
+	);
+}
